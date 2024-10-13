c5d2eec8204e6569cd5ff3da2e898d6b88170545
TFHG <yu.willem@gmail.com>
2024-10-14 01:59:25 +0200

    added change to the app.test.js with hi world

--- test/app.test.js
+++ test/app.test.js
@@ -4,7 +4,7 @@ const app = require('../app'); // Assuming app.js exports the Express instance
 describe('GET /', () => {
     it('should return Hello World', async () => {
         const res = await request(app).get('/');
-        expect(res.text).toBe('Hello World');
+        expect(res.text).toBe('Hi World');
         expect(res.statusCode).toBe(200);
     });
 });
